ccb76231f1427abad2f034c85ccffa3a8ebe9316
白桦 <afunx.bai@ubtrobot.com>
2017-06-06 10:29:44 +0800

    add genjar.sh and generate blelitelib_v0.8.3.jar

--- genjar.sh
+++ genjar.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-path='blelitelib/build/intermediates/bundles/release/classes.jar'
-version='v0.8.1'
+path='blelitelib/build/intermediates/bundles/debug/classes.jar'
+version='v0.8.3'
 cp $path ./"blelitelib_$version.jar"
