ac6a4633c146e52b74d4a69293cad3bf9defa781
白桦 <afunx.bai@ubtrobot.com>
2017-06-09 16:43:22 +0800

    blelitelib_v0.8.4 is ready

--- genjar.sh
+++ genjar.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-path='blelitelib/build/intermediates/bundles/debug/classes.jar'
-version='v0.8.3'
+path='blelitelib/build/intermediates/bundles/default/classes.jar'
+version='v0.8.4'
 cp $path ./"blelitelib_$version.jar"
