cea0385499f2110ae1020075b3c0cf477b5bb28e
白桦 <afunx.bai@ubtrobot.com>
2017-07-04 19:59:51 +0800

    add authority request in ScanActivity.java

--- app/src/main/java/com/afunx/ble/blelitelib/activity/ScanActivity.java
+++ app/src/main/java/com/afunx/ble/blelitelib/activity/ScanActivity.java
@@ -1,8 +1,14 @@
 package com.afunx.ble.blelitelib.activity;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,6 +23,10 @@ import com.afunx.ble.blelitelib.bean.BleDevice;
 import com.afunx.ble.blelitelib.proxy.BleProxy;
 import com.afunx.ble.blelitelib.scanner.BleScanner;
 import com.afunx.ble.blelitelib.test.TestActivity;
+import com.afunx.ble.blelitelib.utils.BleUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ScanActivity extends AppCompatActivity {
@@ -26,6 +36,62 @@ public class ScanActivity extends AppCompatActivity {
     private BleDeviceAdapter mBleDeviceAdapter;
     private Handler mHandler;
     private ListView mListView;
+
+    private int REQUEST_CODE = 1;
+
+    private void requestBleAuthority() {
+
+        List<String> permissions = new ArrayList<>();
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH) != PackageManager.PERMISSION_GRANTED) {
+                permissions.add(Manifest.permission.BLUETOOTH);
+            }
+        }
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_ADMIN) != PackageManager.PERMISSION_GRANTED) {
+                permissions.add(Manifest.permission.BLUETOOTH_ADMIN);
+            }
+        }
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+            }
+        }
+
+        if (!permissions.isEmpty()) {
+            String[] requestPermissions = new String[permissions.size()];
+            int offset = 0;
+            for (String permission : permissions) {
+                requestPermissions[offset++] = permission;
+            }
+            ActivityCompat.requestPermissions(this,
+                    requestPermissions,
+                    REQUEST_CODE);
+        } else {
+            doRefresh();
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE) {
+            boolean isForbidden = false;
+            for (int i = 0; i < grantResults.length; i++) {
+                if (grantResults[i] != 0) {
+                    isForbidden = true;
+                    break;
+                }
+            }
+            if (isForbidden) {
+                Toast.makeText(this, "无法获得蓝牙相关全部权限", Toast.LENGTH_LONG).show();
+            } else {
+                doRefresh();
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,8 +143,10 @@ public class ScanActivity extends AppCompatActivity {
             }
         };
 
+        BleUtils.enableBluetooth();
+
         BleProxy.getInstance().init(getApplicationContext());
-        doRefresh();
+        requestBleAuthority();
     }
 
     private void doRefresh() {
